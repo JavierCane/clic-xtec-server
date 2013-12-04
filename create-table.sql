f84099afd1a42d5c2e8fc1b4dee6c20c6989fe4a
Carlesgb <carlesguiverbou@gmail.com>
2013-12-04 08:40:33 +0100

    Sql query to create the db table.

--- create-table.sql
+++ create-table.sql
@@ -1,50 +1,8 @@
-//versió antiga
-
-CREATE TABLE
-DROIDCLIC_clics 
-(
-id INT/* primary key*/,
-titol varchar(255),
-descripcio varchar(2048),
-lang varchar(2),
-autors varchar(255),
-llicencia varchar(255),
-nivell varchar(255),
-area varchar(255),
-logoUrl varchar(255),
-urlBase varchar(255),
-inst varchar(255),
-clicPrincipal varchar(255),
-clicsAdicionals varchar(1024),
-tipusActivitats varchar(1024)
-)DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
-
-
-//versió nova
-
-CREATE TABLE `DROIDCLIC_clics1` (
+CREATE TABLE `DROIDCLIC_clics` (
 `id` int(11) DEFAULT NULL,
 `titol` varchar(255) DEFAULT NULL,
 `descripcio` varchar(2048) DEFAULT NULL,
 `lang` varchar(2) DEFAULT NULL,
-`lang_de` bit(1) NOT NULL DEFAULT b'0',
-`lang_en` bit(1) NOT NULL DEFAULT b'0',
-`lang_ar` bit(1) NOT NULL DEFAULT b'0',
-`lang_eu` bit(1) NOT NULL DEFAULT b'0',
-`lang_rm` bit(1) NOT NULL DEFAULT b'0',
-`lang_ca` bit(1) NOT NULL DEFAULT b'0',
-`lang_es` bit(1) NOT NULL DEFAULT b'0',
-`lang_eo` bit(1) NOT NULL DEFAULT b'0',
-`lang_fr` bit(1) NOT NULL DEFAULT b'0',
-`lang_gl` bit(1) NOT NULL DEFAULT b'0',
-`lang_el` bit(1) NOT NULL DEFAULT b'0',
-`lang_it` bit(1) NOT NULL DEFAULT b'0',
-`lang_la` bit(1) NOT NULL DEFAULT b'0',
-`lang_oc` bit(1) NOT NULL DEFAULT b'0',
-`lang_pt` bit(1) NOT NULL DEFAULT b'0',
-`lang_ro` bit(1) NOT NULL DEFAULT b'0',
-`lang_sv` bit(1) NOT NULL DEFAULT b'0',
-`lang_zh` bit(1) NOT NULL DEFAULT b'0',
 `autors` varchar(255) DEFAULT NULL,
 `llicencia` varchar(255) DEFAULT NULL,
 `nivell` varchar(255) DEFAULT NULL,
@@ -58,6 +16,13 @@ CREATE TABLE `DROIDCLIC_clics1` (
 `area_ef` bit(1) NOT NULL DEFAULT b'0',
 `area_tec` bit(1) NOT NULL DEFAULT b'0',
 `area_div` bit(1) NOT NULL DEFAULT b'0',
+
+`nivel_all` bit(1) NOT NULL DEFAULT b'0',
+`nivel_INF` bit(1) NOT NULL DEFAULT b'0',
+`nivel_PRI` bit(1) NOT NULL DEFAULT b'0',
+`nivel_SEC` bit(1) NOT NULL DEFAULT b'0',
+`nivel_BTX` bit(1) NOT NULL DEFAULT b'0',
+
 `logoUrl` varchar(255) DEFAULT NULL,
 `urlBase` varchar(255) DEFAULT NULL,
 `inst` varchar(255) DEFAULT NULL,
